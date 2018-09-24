a38f7d8beb02e65acece48517cfdb59e40ac0722
David Battiston <djbatt@live.com>
2018-09-23 23:03:58 -0400

    added shuffle functionality

--- src/components/container.js
+++ src/components/container.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 import Scorebar from './navbar/Scorebar'
 
+// shuffleArray = (array) => {
+//   for (let i = array.length -1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+//   return array;
+// }
+
 export default class Container extends Component {
-  
+
   state={
+    simpsonsArray: [],
     score: 0,
     highScore: 0,
-    input: false
+    input: []
   }
+
+  handleUserInput = (id) => {
+    if (this.state.input.indexOf(id) === -1) {
+      this.setState({ input: this.state.input.concat(id) });
+    }
+  }
+
+  
 
   render() {
     return (
@@ -15,6 +32,8 @@ export default class Container extends Component {
         <Scorebar/>
         <div className='center-align container'>
             <h3 className='Header'>Click and image to start</h3>
+            <div className='Row'>
+            </div>
         </div>
       </div>
     )

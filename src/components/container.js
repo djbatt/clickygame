57582471dd9bf294df130fa4153e59f06b9b6768
David Battiston <djbatt@live.com>
2018-09-23 22:34:15 -0400

    added json and states

--- src/components/container.js
+++ src/components/container.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import Scorebar from './navbar/Scorebar'
 
 export default class Container extends Component {
+  
+  state={
+    score: 0,
+    highScore: 0,
+    input: false
+  }
+
   render() {
     return (
       <div>
